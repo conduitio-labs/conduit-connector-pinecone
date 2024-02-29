50601e169bf7f7e6a5738ecd1eeb5fbeea711941
Adam Haffar <haffaradam@yahoo.com>
2024-02-29 19:24:30 +0000

    add destination config

--- config/config.go
+++ config/config.go
@@ -4,15 +4,14 @@ package config
 // destination. If you don't need shared parameters you can entirely remove this
 // file.
 
+//go:generate paramgen -output=paramgen_dest.go DestinationConfig
+
 const (
 	// ConfigPineconeAPIKey is the config name for Pinecone API key
 	ConfigPineconeAPIKey = "pinecone.apiKey"
 
 	// ConfigPineconeHostURL is the config name for Pinecone Host URL
 	ConfigPineconeHostURL = "pinecone.hostURL"
-
-	// ConfigOpenAIKey is the config name for OpenAI API Key
-	ConfigOpenAIKey = "pinecone.openAIKey"
 )
 
 // Config represents configuration needed for Pinecone
@@ -21,6 +20,9 @@ type Config struct {
 	PineconeAPIKey string `json:"pinecone.apiKey" validate:"required"`
 	// Host URL for Pinecone index.
 	PineconeHostURL string `json:"pinecone.hostURL" validate:"required"`
-	// OpenAI API key for embedding vectors
-	OpenAIKey string `json:"pinecone.openAIKey" validate:"required"`
+}
+
+type DestinationConfig struct {
+	// Config includes parameters that are the same in the source and destination.
+	Config
 }
